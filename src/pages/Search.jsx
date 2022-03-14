aa904458710afa99cf7de361a6b0bb576eb7afec
Matheus <stvgamer74@gmail.com>
2022-03-13 22:53:01 -0300

    R05: Criando formulario com validaçao para ativar botao na Page Search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,55 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.onInputChange = this.onInputChange.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+    this.state = {
+      search: '',
+      isButtonDisabled: true,
+    };
+  }
+
+  onInputChange({ target: { type, name, value, checked } }) {
+    this.setState({
+      [name]: (type === 'checkbox' ? checked : value),
+    }, this.validateForm);
+  }
+
+  validateForm() {
+    const { search } = this.state;
+    const chars = 2;
+    this.setState({
+      isButtonDisabled: search.length < chars,
+    });
+  }
+
   render() {
+    const { search, isButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <span>Search Page</span>
+        <form>
+          <label htmlFor="input-search">
+            <input
+              type="text"
+              id="input-search"
+              name="search"
+              value={ search }
+              onChange={ this.onInputChange }
+              data-testid="search-artist-input"
+            />
+            <button
+              type="submit"
+              disabled={ isButtonDisabled }
+              data-testid="search-artist-button"
+            >
+              Pesquisar
+
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
